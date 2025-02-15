746def13d41a92e8468e5c70510da4037b2f5dc9
programador de elite <leonardo.turque10@gmail.com>
2025-02-14 21:42:39 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
 const CACHE_NAME = 'login-cache-v1';
 const urlsToCache = [
     'index.html',
-    'horario1.html',
     'calendario.html',
+    'horario1.html',
     'livros.html',
     'styles.css',
     'script.js',
@@ -16,31 +16,6 @@ self.addEventListener('install', (event) => {
         caches.open(CACHE_NAME)
             .then((cache) => {
                 return cache.addAll(urlsToCache);
-            })
-    );
-});
-
-// Ativação do Service Worker
-self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cacheName) => {
-                    if (cacheName !== CACHE_NAME) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
-});
-
-// Interceptação de requisições
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
-                return response || fetch(event.request);
             })
     );
 });
