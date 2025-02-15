c3e97b2c4a3da20da81044625cf321036c44bd28
programador de elite <leonardo.turque10@gmail.com>
2025-02-14 21:23:33 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,43 @@
-const CACHE_NAME = 'login'
+const CACHE_NAME = 'login-cache-v1';
+const urlsToCache = [
+    'index.html',
+    'styles.css',
+    'script.js',
+    'icon-192x192.png',
+    'icon-512x512.png'
+];
+
+// Instalação do Service Worker
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Ativação do Service Worker
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Interceptação de requisições
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                return response || fetch(event.request);
+            })
+    );
+});
